a7902fee43446c77b4d9f160514ba03aa5f62da4
antoine-06 <66874686+antoine-06@users.noreply.github.com>
2020-06-14 19:04:00 +0200

    Add files via upload

---
+++ LE SITE DES EMOTIONS/jouer/jouer.js
@@ -0,0 +1,40 @@
+function questions(numero)
+{
+	var chaine="";
+	switch(numero)
+	{
+		case 1:
+		chaine="Comment dit-on oui en Albanie ?*En se curant le nez*En levant son pouce*En hochant la tête de gauche à droite*En toussant*3";
+		break;
+		case 2:
+		chaine="Quelle est la couleur originelle de Facebook ?*Vert et Blanc*Bleu et Blanc*Jaune et Bleu*Rouge et Blanc*2";
+		break;
+		case 3:
+		chaine="Combien de temps dorment les fourmis ?*Une semaine par an*16 minutes par jour*Elles ne dorment pas, elles méditent*5 jours par semaine*2";
+		break;
+		case 4:
+		chaine="Quel est l'objet préféré de la loutre ?*Des sacs chanel*Des playstations*Des pierres*Des bâtons*3";
+		break;
+		case 5:
+		chaine="Qui de Russie ou de Pluton a la plus grande surface ?*La Russie*Pluton*L'Efrei*Elles sont égales*1";
+		break;
+		case 6:
+		chaine="Combien de temps dans sa vie passe-t-on devant un feu rouge ? *6 jours*6 mois*6 années*6 minutes*2";
+		break;
+		case 7:
+		chaine="Il y a assez de fer dans un être humain pour faire : *La tour Eiffel*Une balle*Une clé de porte*Un clou*4";
+		break;
+		case 8:
+		chaine="La corne d'un narval est en réalité : *Son antenne*Son oeil*Sa dent*Son bras*3";
+		break;
+		case 9:
+		chaine="Un américain sur dix pense que HTML est :*Une MST*Une marque de vélo*Un nouveau Virus*Un type de Hamburger*1";
+		break;
+		case 10:
+		chaine="En moyenne, combien de fois fait-on le tour du monde en marchant dans une vie : *2 fois*3 fois*4 fois*5 fois*2";
+		break;
+		
+	}
+	
+	return chaine;
+}
